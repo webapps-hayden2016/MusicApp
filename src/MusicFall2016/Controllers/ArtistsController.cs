31cbdfb42fc00330d48cc47aac8f6d8d0d400364
webapps-hayden2016 <webapps-hayden2016@gmail.com>
2016-10-21 16:05:26 -0400

    DB Working

---
+++ src/MusicFall2016/Controllers/ArtistsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusicFall2016.Models;
+
+namespace MusicFall2016.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private readonly MusicDbContext _context;
+
+        public ArtistsController(MusicDbContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var artists = _context.Artists.ToList();
+            return View(artists);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Artist artist)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Artists.Add(artist);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+    }
+}
